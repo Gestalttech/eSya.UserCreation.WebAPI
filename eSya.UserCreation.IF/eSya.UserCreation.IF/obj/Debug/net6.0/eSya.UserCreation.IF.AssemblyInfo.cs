141a7dd6b253c489f79ed3e88c718ea627fd7bbc
AbdulR <abdulr@gestalttechindia.in>
2024-09-09 17:16:09 +0530

    User Approval forms added

--- eSya.UserCreation.IF/eSya.UserCreation.IF/obj/Debug/net6.0/eSya.UserCreation.IF.AssemblyInfo.cs
+++ eSya.UserCreation.IF/eSya.UserCreation.IF/obj/Debug/net6.0/eSya.UserCreation.IF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSya.UserCreation.IF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40012a07553ad5c34223ad1a5955575cce366dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde06c2f89fe357e505e735f7875caeabd783260")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSya.UserCreation.IF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSya.UserCreation.IF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
